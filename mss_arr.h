dcae072754fa3a77f1bd093ffa5f1b0b09c728d5
yunsik22 <choiy@onid.orst.edu>
2015-07-11 23:32:49 -0700

    mss_arr.h

---
+++ mss_arr.h
@@ -0,0 +1,27 @@
+#ifndef F_MSS_ARR_H
+#define F_MSS_ARR_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int mss_enum(int *arr, int size);
+int mss_enum(int *arr, int size, int *l, int *h);
+int mss_enum2(int *arr, int size);
+int mss_enum2(int *arr, int size, int *l, int *h);
+int mss_divconq(int *arr, int l, int h);
+void mss_divconq(int *arr, int l, int h, int *sum, int *l_idx, int *h_idx);
+int mss_cross(int *arr, int l, int m, int h);
+void mss_cross(int *arr, int l, int h, int *sum, int *l_idx, int *h_idx);
+int mss_linear(int *arr, int size);
+int mss_linear(int *arr, int size, int *l, int *h);
+int max(int a, int b);
+int getlines(std::ifstream &fin, std::vector<std::string> &vstr);
+void vec2d_int(std::vector <std::vector<int> > &vint, std::vector<std::string> &vstr);
+void output(std::string &fname, std::vector <std::vector<int> > &vint, int *vecsize, int **arr);
+void output(std::string &fname, std::vector <std::vector<int> > &vint, int *vecsize, int *maxsum_arr, int **sub_arr, int*);
+void input_random(int **arrs_r, int *arr_size, int size);
+void experiment(int *arr_size, int size);
+
+#endif /* F_MSS_ARR_H */
